3157907f4a5d2fed42132c0172bafa6db3946db1
gitHaq2531 <afzaulhaque1@gmail.com>
2024-08-25 18:45:59 +0530

    new update

--- shoProd/src/test/java/com/client/shoProd/module/Flipkart.java
+++ shoProd/src/test/java/com/client/shoProd/module/Flipkart.java
@@ -2,14 +2,10 @@ package com.client.shoProd.module;
 
 import java.io.IOException;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WindowType;
-import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.client.shoProd.generic.baseUtility.BaseClassUtils;
-import com.client.shoProd.generic.webdriverUtility.webDriverUtils;
 
 @Listeners(com.client.shoProd.generic.ListenersUtility.ListenersImpClass.class)
 public class Flipkart extends BaseClassUtils
